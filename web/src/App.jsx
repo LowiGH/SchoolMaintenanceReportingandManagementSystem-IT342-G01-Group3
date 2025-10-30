0cfa66ab4d3f7ba99ab92b2e94d7898011b55c08
LowiGH <louiebajamunde5@gmail.com>
2025-10-30 15:44:35 +0800

    Update ProfilePage layout no main or dashboard yet so its fine.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,16 @@
-import { LoginPage } from './components/pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./components/pages/LoginPage";
+import ProfilePage from "./components/pages/ProfilePage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
